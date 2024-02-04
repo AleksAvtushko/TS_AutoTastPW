45c03c5868ef4d3d1c85da246f44ecd7f04bdef4
Aleksander Avtushko <aleksandravtusko_gmail_com>
2024-02-04 14:36:28 +0300

    feat: added tests for logIn page

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,8 +28,6 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
-        actionTimeout: 10 * 1000,
-        navigationTimeout: 30 * 1000,
     },
 
     /* Configure projects for major browsers */
---
+++ tests/loginPageTests.spec.ts
@@ -0,0 +1,52 @@
+import { test, expect } from "@playwright/test";
+import { logInPageData } from "../fixstures/logInPageData";
+
+test.describe("Check LogIn page", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto(logInPageData.urlMarketingPage);
+    });
+
+    test("Check: Login to site if Email and Password are valid", async ({ page }) => {
+        await page.getByRole("button", { name: "Login" }).click();
+        await page.getByTestId("username").fill(logInPageData.validEmail);
+        await page.getByTestId("password").fill(logInPageData.validPassword);
+        await page.getByRole("button", { name: "Sign In" }).click();
+        await expect(page).toHaveURL(logInPageData.urlCalendarPage, { timeout: 20000 });
+    });
+
+    test("Check: Error message is displayed if Email is invalid", async ({ page }) => {
+        await page.getByRole("button", { name: "Login" }).click();
+        await page.getByTestId("username").fill(logInPageData.invalidEmail);
+        await page.getByTestId("password").fill(logInPageData.validPassword);
+        await page.getByRole("button", { name: "Sign In" }).click();
+        await expect(page.getByRole("dialog")).toContainText("Incorrect username or password.");
+    });
+
+    test("Check: Error message is displayed if Password is invalid", async ({ page }) => {
+        await page.getByRole("button", { name: "Login" }).click();
+        await page.getByTestId("username").fill(logInPageData.validEmail);
+        await page.getByTestId("password").fill(logInPageData.invalidPassword);
+        await page.getByRole("button", { name: "Sign In" }).click();
+        await expect(page.getByRole("dialog")).toContainText("Incorrect username or password.");
+    });
+
+    test("Check: Error message is displayed if Email and Password are invalid", async ({ page }) => {
+        await page.getByRole("button", { name: "Login" }).click();
+        await page.getByTestId("username").fill(logInPageData.invalidEmail);
+        await page.getByTestId("password").fill(logInPageData.invalidPassword);
+        await page.getByRole("button", { name: "Sign In" }).click();
+        await expect(page.getByRole("dialog")).toContainText("Incorrect username or password.");
+    });
+
+    test("Check: Sign In button is disabled if Email field is empty", async ({ page }) => {
+        await page.getByRole("button", { name: "Login" }).click();
+        await page.getByTestId("password").fill(logInPageData.invalidPassword);
+        await expect(page.getByRole("button", { name: "Sign In" })).toHaveAttribute("disabled");
+    });
+
+    test("Check: Sign In button is disabled if Password field is empty", async ({ page }) => {
+        await page.getByRole("button", { name: "Login" }).click();
+        await page.getByTestId("username").fill(logInPageData.invalidEmail);
+        await expect(page.getByRole("button", { name: "Sign In" })).toHaveAttribute("disabled");
+    });
+});
