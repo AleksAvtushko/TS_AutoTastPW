c6d5cef78c8c40f577458d27ea7d8b74732a8297
Aleksander Avtushko <aleksandravtusko_gmail_com>
2024-02-04 12:02:35 +0300

    feat: added logInPageData

---
+++ fixstures/logInPageData.ts
@@ -0,0 +1,9 @@
+export enum logInPageData {
+    validEmail = "ilya.stupen+1@imbuesystems.com",
+    validPassword = "",
+    invalidEmail = "ilya.stupenWrongEmail@imbuesystems.com",
+    invalidPassword = "incorectPassword!@#$%123",
+    urlCalendarPage = "https://dev.toothapps.com/Site/Home/Calendar",
+    urlMarketingPage = "https://dev.toothapps.com/Marketing/Home",
+    urlDemoPage = "https://dev.toothapps.com/Marketing/Home/getDemo",
+}
